757ce09f4195823558aec25cac6bc7fc2807235b
Abr4ha <aj.sanchez.o@edu.utc.mx>
2021-11-03 11:01:02 -0600

    Entrada de Datos

---
+++ 02_Entrada_Datos/EntradaTexto.java
@@ -0,0 +1,15 @@
+class EntradaTexto{
+    
+    //Cuerpo del Programa o de la clase
+
+    public static void main(String[] args){
+        String nombre;
+
+        System.out.println("Por favor Introduce tu Nombre: ");
+
+        nombre = System.console().readLine();
+
+        System.out.println("Hola, "+ nombre + " Bienvenido a tu segundo programa");
+    }
+    
+}
