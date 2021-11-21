2006f1381551ce007d78484025bcd8ae204a2d85
Abr4ha <aj.sanchez.o@edu.utc.mx>
2021-11-20 23:58:55 -0600

    Cuadro magico

--- 06_Clases_metodos/Ejercicios.java
+++ 06_Clases_metodos/Ejercicios.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
 public class Ejercicios {
     char op;
     boolean error;
+    int n, m, e;
     Scanner entrada = new Scanner(System.in);    
     public void menu(){
         do{
@@ -23,6 +26,35 @@ public class Ejercicios {
             case 'c':
                 break;
             case 'd':
+                System.out.println("Introduce el numero de columnas :");
+                n = entrada.nextInt();
+                System.out.println("Introduce el numero de filas :");
+                m = entrada.nextInt();
+                if( n%2==0){
+                    e = n/2;
+                }else{
+                    e = n;
+                    if( e >= 3){
+                        e = (n-1)/2;
+                    }
+                }
+                for(int i = 1; i<=e; i++){
+                    for(int j = 1; j <= m; j++){
+                        System.out.print("* ");
+                    }
+                    System.out.println(" ");
+                    if( e >= 2){
+                        for(int l = 1; l<=m; l++){
+                            System.out.print("/ ");
+                        }
+                        System.out.println(" ");
+                    }
+                }
+                if( e%2==0){
+                    for(int j = 1; j <= m; j++){
+                        System.out.print("* ");
+                    }
+                }
                 break;
             default:
                 error=true;
