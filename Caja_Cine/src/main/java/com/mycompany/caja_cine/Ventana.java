04821bc723a524641238389058f2fdd8f3d84617
Abr4ha <aj.sanchez.o@edu.utc.mx>
2021-12-15 22:26:07 -0600

    Idea

---
+++ Caja_Cine/src/main/java/com/mycompany/caja_cine/Ventana.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.caja_cine;
+
+/**
+ *
+ * @author hyexzc
+ */
+public interface Ventana {
+    
+}
