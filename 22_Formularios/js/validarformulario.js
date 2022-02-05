06b3f67b6a6f78e898dbc7cfe98f674b4565b085
Abr4ha <aj.sanchez.o@edu.utc.mx>
2022-02-05 15:14:04 -0600

    Formulario

--- 22_Formularios/js/validarformulario.js
+++ 22_Formularios/js/validarformulario.js
@@ -1,18 +1,18 @@
 function validar(formulario){
     //validar el tamaño del nombre
-    if(formulario.nombre.value.lenght < 5){
+    if(formulario.nombre.value.length < 5){
         alert("Escriba por lo menos seis caracteres en el campo nombre");
         //colocar cursor en el campo de error
         formulario.nombre.focus();
-        return(false);
+        return (false);
     }
     //se cambio var a let
-    var checkOK="ABCDEFGHIJKLMNÑOPKRSTUVWXYZabcdefghijklmnñopkrstuvwxyz";
-    var checkStr= formulario.nombre.value;
-    var allvalid= true;
-    for(var i = 0; i < checkStr.lenght; i++){
+    var checkOK ="ABCDEFGHIJKLMNOPKRSTUVWXYZabcdefghijklmnopkrstuvwxyz";
+    var checkStr = formulario.nombre.value;
+    var allvalid = true;
+    for(var i = 0; i < checkStr.lenght; i++ ){
         var ch = checkStr.charAt(i);
-        for(var j=0; j < checkOK.length; j++){
+        for(var j = 0; j < checkOK.length; j++ ){
             if(ch == checkOK.charAt(j)){
                 break;
             }
